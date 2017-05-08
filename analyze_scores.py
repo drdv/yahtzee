525055ee6617fa33c559d08bcdffd87267ad3d08
drdv <mail.mitko@gmail.com>
2017-05-08 19:28:05 +0200

    analyze_scores.py: fix best score and add average score

--- analyze_scores.py
+++ analyze_scores.py
@@ -17,25 +17,42 @@ class player():
         self.win   = [];
         self.loose = [];
         self.draw  = [];
-        self.best_score = 0
+        self.best_score      = 0
+        self.total_score     = 0
+        self.number_of_games = 0
 
-    def set(self, winners, loosers, score):
+    def set(self, winners, loosers):
         if self.name in winners:
-            self.win.extend(loosers)
-            self.draw.extend([w for w in winners if w != self.name])
+            self.win.extend(list(loosers))
+            self.draw.extend([key for key in winners.keys() if key != self.name])
 
-            if self.best_score < score:
-                self.best_score = score
+            if self.best_score < winners[self.name]:
+                self.best_score = winners[self.name]
+
+            self.total_score += winners[self.name]
+            self.number_of_games += 1
 
         if self.name in loosers:
-            self.loose.extend(winners)
-            self.draw.extend([w for w in loosers if w != self.name])
+            self.loose.extend(list(winners))
+            self.draw.extend([key for key in loosers.keys() if key != self.name])
 
-    def result_against(self, opponent):
+            if self.best_score < loosers[self.name]:
+                self.best_score = loosers[self.name]
+
+            self.total_score += loosers[self.name]
+            self.number_of_games += 1
+
+
+    def show_result_against(self, opponent):
         W = len([x for x in self.win   if x == opponent.name])
         L = len([x for x in self.loose if x == opponent.name])
 
         print('{0:17s}: {1:2d} - {2:2d}'.format(self.name + ' vs. ' + opponent.name, W, L))
+
+    def show_score(self):
+        print("{0:17s}: {1:2.2f}  ({2:3d})".format(self.name,
+                                                   self.total_score/self.number_of_games,
+                                                   self.best_score))
 
 # ===========================================================================
 
@@ -47,25 +64,25 @@ for file in glob.glob(filename_wildcard):
     with open(file) as data_file:
         data = json.load(data_file)
 
-    key = list(data['total-score'].keys())
-    val = list(data['total-score'].values())
-    max_score = max(val)
+    scores = data['total-score']
+    max_score = max(scores.values())
 
-    winners = [key[i] for i,j in enumerate(val) if j == max_score]
-    loosers = [key[i] for i,j in enumerate(val) if j != max_score]
+    winners = dict([(key, val) for (key, val) in scores.items() if val == max_score])
+    loosers = dict([(key, val) for (key, val) in scores.items() if val != max_score])
 
     for player in players:
-        player.set(winners, loosers, max_score)
+        player.set(winners, loosers)
 
 # ===========================================================================
 
 # display scores
 print("===============================")
 for (player_i, player_j) in list(itertools.combinations(players,2)):
-    player_i.result_against(player_j)
+    player_i.show_result_against(player_j)
 print("===============================")
+print("                   average  max")
 for player in players:
-    print("{0:17s}: {1:3d}".format(player.name, player.best_score))
+    player.show_score()
 print("===============================")
 
 ###EOF
