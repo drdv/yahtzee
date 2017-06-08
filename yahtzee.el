518533b884d6665ed77b45112c91a0719defabdb
drdv <mail.mitko@gmail.com>
2017-06-08 17:36:25 +0200

    set yahtzee-selected-field to nil in yahtzee-reset

--- yahtzee.el
+++ yahtzee.el
@@ -706,6 +706,7 @@ A bonus is awarded when the player scores at least
   (setq yahtzee-moves-left (length yahtzee-fields-alist))
   (setq yahtzee-game-start-time (current-time))
   (setq yahtzee-dice-outcomes-fixed nil)
+  (setq yahtzee-selected-field nil)
 
   (setq yahtzee-dice-outcomes
 	(make-vector yahtzee-number-of-dice-to-throw nil))
